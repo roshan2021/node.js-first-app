6208ef7bfce7952d8bf2f5bb8dece89a0e2cabcf
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-08-03 20:28:42 -0500

    HTTP

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
+//HTTP
+const http = require('http');
 
-//Event
-const EventEmitter = require('events');
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.write('Hello world');
+        res.end();
+    }
 
-const Logger = require('./logger');
-const logger = new Logger();
-
-//Register a listener
-logger.on('messageLogged', (arg) => {
-    console.log('Listener called', arg);
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
 });
 
-logger.log('message');
+server.listen(3000);
+
+console.log('Listening on port 3000...');
+
+
+// //Event
+// const EventEmitter = require('events');
+//
+// const Logger = require('./logger');
+// const logger = new Logger();
+//
+// //Register a listener
+// logger.on('messageLogged', (arg) => {
+//     console.log('Listener called', arg);
+// });
+//
+// logger.log('message');
 
 
 // //File System
