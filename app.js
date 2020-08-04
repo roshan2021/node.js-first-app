f391e95a5b8ff4aa63c7e15aa0dfee93c56f7531
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-08-03 19:37:00 -0500

    operating system using node. js

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+//Operating System
+const os = require('os');
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+
+//Template string
+//ES6 / ES2015 : ECMAScript 6
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
